7178fe4dc9533793ce1a7c6122c8e2dc698934be
bdcruz727 <138246577+bdcruz727@users.noreply.github.com>
2024-09-28 12:42:37 -0700

    base game
    player input

---
+++ faster-than-light-yagami/Scripts/doneButton.gd
@@ -0,0 +1,17 @@
+extends Button
+@onready var input_control: TextEdit = $"../inputControl"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+	
+	
