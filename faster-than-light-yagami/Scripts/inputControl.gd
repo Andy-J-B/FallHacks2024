72336d054d619cdd529f25550051c0733e0ac0d7
bdcruz727 <138246577+bdcruz727@users.noreply.github.com>
2024-09-28 17:15:08 -0700

    Difficulty Added

--- faster-than-light-yagami/Scripts/inputControl.gd
+++ faster-than-light-yagami/Scripts/inputControl.gd
@@ -19,10 +19,12 @@ extends LineEdit
 var letters_typed: int = 0
 var old_names: Array = [-1]
 var names:Dictionary = NameDataBase.englishNames
+var hardNames:Dictionary = NameDataBase.hardEnglishNames
 #var names: Dictionary = {0: "Benny Maxwell", 1:"Farmer John", 2:"Bryan Dela Cruz"}
 var current_name: String = ""
 var score:int = 0
 var death: bool = false
+var difficulty:int = 0
 
 var red: String = "#EE4B2B"
 var grey: String = "#808080"
@@ -31,8 +33,8 @@ var green: String = "#7CFC00"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	current_name = get_new_name(names, old_names)
-	print(current_name)
+	current_name = get_new_name(names, hardNames, old_names, difficulty)
+	#print(current_name)
 	player_input.modulate = "808080"
 	player_input.placeholder_text = current_name
 	
@@ -47,12 +49,20 @@ func _process(delta: float) -> void:
 	if death == false:
 		lives_saved.text = "Lives Saved: " + str(score)
 		curr_name.text = current_name
-		print(timer.time_left)
+		#print(timer.time_left)
 		if timer.time_left <= 0:
 			death = true
-			print("Death")
 			if !death_sound.playing:
 				death_sound.play()
+	if death == true:
+		player_input.editable = false
+		player_input.visible = false
+		done_label.visible = false
+		done_button.visible = false
+		curr_name.visible = false
+		timer.stop()
+		lives_saved.visible = false
+		
 		
 	
 	pass
@@ -68,30 +78,22 @@ func check_match(source: String, input: String, length:int) -> bool:
 	
 		
 	
-func get_new_name(names:Dictionary, old_names:Array) -> String:
+func get_new_name(names:Dictionary, hardNames:Dictionary, old_names:Array, difficulty:int) -> String:
 	var size = names.size()
+	var hardSize = hardNames.size()
 	var random_key = -1
+	
+	# Hard Name Selected
+	if randi_range(0, 10) < difficulty:
+		random_key = hardNames.keys()[randi() % hardSize]
+		return hardNames[random_key]
+		
+	
 	while random_key in old_names:
 		random_key = names.keys()[randi() % size]
 	return names[random_key]
 	
 
-"""
-func _on_done_button_pressed() -> void:
-	if current_name == player_input.text:
-		score += 1
-		done_label.text = "SUCCESS"
-		done_label.modulate = green
-		var old_key = names.find_key(current_name)
-		old_names.append(old_key)
-		current_name = get_new_name(names, old_names)
-		player_input.text = ""
-		player_input.placeholder_text = current_name
-		player_input.modulate = "#808080"
-	else:
-		done_label.modulate = red
-		done_label.text = "FAILURE"
-"""
 		
 	
 		
@@ -108,6 +110,7 @@ func _on_text_changed(new_text: String) -> void:
 
 func _on_text_submitted(new_text: String) -> void:
 	if current_name == player_input.text:
+		print(difficulty)
 		score += 1
 		
 		done_label.text = "SUCCESS"
@@ -115,12 +118,15 @@ func _on_text_submitted(new_text: String) -> void:
 		done_label.modulate = green
 		var old_key = names.find_key(current_name)
 		old_names.append(old_key)
-		current_name = get_new_name(names, old_names)
+		current_name = get_new_name(names, hardNames, old_names, difficulty)
 		player_input.text = ""
 		player_input.placeholder_text = current_name
 		player_input.modulate = "#808080"
-		if (timer.wait_time >= 3):
-			timer.wait_time = 10 - score/6
+		if difficulty <8:
+			if score % 6 == 0:
+				difficulty += 1
+		if (timer.wait_time >= 5):
+			timer.wait_time = 15 - score/6
 		timer.start()
 	else:
 		done_button.modulate = red
