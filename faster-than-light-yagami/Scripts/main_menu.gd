6c4f0075fc1295c922d239fa9db2b3682ce831c1
KaiHikichi <kai.hikichi@gmail.com>
2024-09-28 17:16:04 -0700

    game over screen and buttons

--- faster-than-light-yagami/Scripts/main_menu.gd
+++ faster-than-light-yagami/Scripts/main_menu.gd
@@ -4,7 +4,7 @@ extends Node
 
 
 func _on_play_pressed():
-	#get_tree().change_scene_to_file("")
+	get_tree().change_scene_to_file("res://Scenes/type.tscn")
 	pass
 	
 func _on_leaderboard_pressed():
